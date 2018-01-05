3357099b579eb94446644190589f3318df5d9f2d
guoxiaopang <200832147@qq.com>
2018-01-05 16:43:19 +0800

    提交部分代码

--- src/global/App.go
+++ src/global/App.go
@@ -1,1 +1,32 @@
 package global
+
+import (
+	"ini"
+	"time"
+)
+
+const (
+	// Debug 开发版本
+	Debug = "Debug"
+	// ReleaseDebug 外测版本
+	ReleaseDebug = "ReleaseDebug"
+	// Release 正式版
+	Release = "Release"
+)
+
+type app struct {
+	Name       string
+	Version    string
+	LaunchTime time.Time
+	Date       time.Time
+	Uptime     time.Duration
+	Env        string
+}
+
+// App 初始化app
+var App = &app{}
+
+func init() {
+	cfg, err := ini.InsensitiveLoad("env.ini")
+	App.LaunchTime = time.Now()
+}
